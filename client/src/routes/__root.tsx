a0c61acc2b4ecc54b26c99727489466fcdfdca47
detarkende <dkende4@gmail.com>
2026-01-03 13:47:54 +0100

    create new client

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,31 @@
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
+
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
+
+import type { QueryClient } from '@tanstack/react-query';
+
+interface MyRouterContext {
+  queryClient: QueryClient;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+          TanStackQueryDevtools,
+        ]}
+      />
+    </>
+  ),
+});
---
+++ client/vite.config.ts
@@ -0,0 +1,35 @@
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+
+const SERVER_URL = 'http://localhost:3000';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: { '/api': SERVER_URL, '/manifest.json': SERVER_URL },
+  },
+
+  plugins: [
+    devtools(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
